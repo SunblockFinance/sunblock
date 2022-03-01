648c51b4ef018ee8dca11b54950b5a0d4db959f9
Kenth Fagerlund <kenth@divit.com.hk>
2022-03-01 22:13:46 +0800

    Crypto icons

--- utils/formaters.ts
+++ utils/formaters.ts
@@ -4,12 +4,22 @@ import { BigNumber, ethers } from "ethers"
 
 // SPDX-License-Identifier: MIT
 export const formatWeiToString = (wei:BigNumber): string => {
-    const eth = ethers.utils.formatEther(wei)
-    return (+eth).toFixed(4)
+    try {
+        const eth = ethers.utils.formatEther(wei)
+        return (+eth).toFixed(4)
+    } catch (error:any) {
+        console.log(error)
+    }
+    return ""
 }
 
 export const formatWeiToNumber = (wei:BigNumber): number => {
+   try {
     const weiString = formatWeiToString(wei)
     return Number(weiString)
+   } catch (error:any) {
+       console.log(error)
+   }
+    return 0
 
 }
