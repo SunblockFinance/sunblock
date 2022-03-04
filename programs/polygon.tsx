693bccdc0c76720bb54e62d687d8c1512508479e
Kenth Fagerlund <kenth@divit.com.hk>
2022-03-04 14:38:33 +0800

    Prod settings

--- programs/polygon.tsx
+++ programs/polygon.tsx
@@ -2,17 +2,17 @@
 /**
  * Address to main contract for sunblock
  */
-export const CONTRACT_ADDRESS_SUNBLOCK = '0x3c231587123b174bAFa2B3933AFbE4f7344fBdE5'
+export const CONTRACT_ADDRESS_SUNBLOCK = '0xBF901Eb7354a22a8746c91E80d1e5D34C4D0596a'
 
 /**
  * Adress to DEMO ERC20 Token on testnet
  */
-export const TOKEN_ADDRESS_DEMOERC20 = '0xfb5DE7AC1B0F405332d9DB97Bbaa07175631FA5F'
+export const TOKEN_ADDRESS_DEMOERC20 = '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174'
 
 /**
  * Currently supported block chain
  */
-export const CHAINID = 80001
+export const CHAINID = 137
 
 export const REWARD_WALLET = '0x19697686F5aC6d5e55E0DE72214471599245D230'
 export const INVESTMENT_WALLET = '0x1FFe3875819d27EFD9b0ece0E3E0C8a87e3E73cb'
