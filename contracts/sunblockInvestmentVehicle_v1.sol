1d61f5058f8aac0a015d5024945562de19428274
Kenth Fagerlund <kenth@divit.com.hk>
2022-03-08 21:30:16 +0800

    Basic testing set

--- contracts/sunblockInvestmentVehicle_v1.sol
+++ contracts/sunblockInvestmentVehicle_v1.sol
@@ -1,41 +1,70 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
+import '@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol';
 
 /// @custom:security-contact security@sunblock.finance
-contract InvestmentVehicle is Initializable, PausableUpgradeable, AccessControlUpgradeable, UUPSUpgradeable {
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+contract InvestmentVehicle is
+  Initializable,
+  PausableUpgradeable,
+  AccessControlUpgradeable,
+  UUPSUpgradeable
+{
+  bytes32 public constant PAUSER_ROLE = keccak256('PAUSER_ROLE');
+  bytes32 public constant UPGRADER_ROLE = keccak256('UPGRADER_ROLE');
+  bytes32 public constant MANAGER_ROLE = keccak256('MANAGER_ROLE');
 
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() initializer {}
+  IERC20 paymentInstrument; // Token used to be used for investment and reward. This will be later bridged and swapped to vehicle token
+  uint256 public managementFee; // Fee taken from the reward prior to distribution. No fee for investment pool. EVER.
 
-    function initialize() initializer public {
-        __Pausable_init();
-        __AccessControl_init();
-        __UUPSUpgradeable_init();
+  // ========= EVENTS =========== //
+  event InvestmentDeposited(address from, address by, uint256 amount);
 
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(PAUSER_ROLE, msg.sender);
-        _grantRole(UPGRADER_ROLE, msg.sender);
-    }
+  /// @custom:oz-upgrades-unsafe-allow constructor
+  constructor() initializer {}
 
-    function pause() public onlyRole(PAUSER_ROLE) {
-        _pause();
-    }
+  function initialize(address instrument, uint256 fee) public initializer {
+    __Pausable_init();
+    __AccessControl_init();
+    __UUPSUpgradeable_init();
 
-    function unpause() public onlyRole(PAUSER_ROLE) {
-        _unpause();
-    }
+    _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    _grantRole(PAUSER_ROLE, msg.sender);
+    _grantRole(UPGRADER_ROLE, msg.sender);
+    _grantRole(MANAGER_ROLE, msg.sender);
 
-    function _authorizeUpgrade(address newImplementation)
-        internal
-        onlyRole(UPGRADER_ROLE)
-        override
-    {}
+    paymentInstrument = IERC20(instrument);
+    managementFee = fee;
+  }
+
+
+  function depositInvestment(address invPool, uint256 amount) external onlyRole(MANAGER_ROLE) {
+      bool success = paymentInstrument.transferFrom(invPool, address(this), amount);
+      require(success);
+      emit InvestmentDeposited(invPool, msg.sender, amount);
+  }
+  function withdrawInvestment() external onlyRole(MANAGER_ROLE) {}
+  function depositReward() external onlyRole(MANAGER_ROLE) {}
+  function withdrawReward() external onlyRole(MANAGER_ROLE) {}
+  function _extractFee() internal {}
+
+
+  function pause() public onlyRole(PAUSER_ROLE) {
+    _pause();
+  }
+
+  function unpause() public onlyRole(PAUSER_ROLE) {
+    _unpause();
+  }
+
+  function _authorizeUpgrade(address newImplementation)
+    internal
+    override
+    onlyRole(UPGRADER_ROLE)
+  {}
 }
---
+++ test/InvestmentVehicle/configuration-test.js
@@ -0,0 +1,60 @@
+// Copyright 2022 Kenth Fagerlund.
+// SPDX-License-Identifier: MIT
+const { expect } = require('chai')
+const { ethers, upgrades } = require('hardhat')
+
+let vehicleContract
+let usdcContract
+let owner
+let addr1
+let addr2
+let addrs
+let MANAGER_ROLE
+let UPGRADER_ROLE
+let PAUSER_ROLE
+
+beforeEach(async function () {
+  ;[owner, addr1, addr2, ...addrs] = await ethers.getSigners()
+
+  MANAGER_ROLE = ethers.utils.id('MANAGER_ROLE')
+  UPGRADER_ROLE = ethers.utils.id('UPGRADER_ROLE')
+  PAUSER_ROLE = ethers.utils.id('PAUSER_ROLE')
+
+  const USDC = await ethers.getContractFactory('USDCToken')
+  usdcContract = await USDC.deploy()
+  await usdcContract.deployed()
+
+  // Get the ContractFactory and Signers here.
+  const Vehicle = await ethers.getContractFactory('InvestmentVehicle')
+  vehicleContract = await upgrades.deployProxy(
+    Vehicle,
+    [usdcContract.address, 100],
+    { initializer: 'initialize', kind: 'uups' }
+  )
+  await vehicleContract.deployed()
+})
+
+describe('InvestmentVehicle tests', function () {
+  it('Should have creator as manager role', async () => {
+    expect(await vehicleContract.hasRole(MANAGER_ROLE, owner.address)).to.equal(
+      true
+    )
+  })
+
+  it('Should have creator as upgrader role', async () => {
+    expect(await vehicleContract.hasRole(UPGRADER_ROLE, owner.address)).to.equal(
+      true
+    )
+  })
+
+  it('Should have creator as pauser role', async () => {
+    expect(await vehicleContract.hasRole(PAUSER_ROLE, owner.address)).to.equal(
+      true
+    )
+  })
+})
+
+// let tx: ContractTransaction = await myToken.connect(accounts[0]).transfer(accounts[1].address, 1);
+
+// let receipt: ContractReceipt = await tx.wait();
+// console.log(receipt.events?.filter((x) => {return x.event == "Transfer"}));
